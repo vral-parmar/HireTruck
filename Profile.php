a2781f4d8a039ecdccd6e18339a5e7a323e7ad2a
parmarviral <parmarviral93@gmail.com>
2019-02-20 11:39:14 +0530

    profile

--- Profile.php
+++ Profile.php
@@ -71,7 +71,110 @@ require_once("Session.php");
                                 <div class="col-xs-12 col-sm-4">
                                     <h2><strong> 20,7K </strong></h2>
                                     <p><small>Followers</small></p>
-                                    <button class="btn btn-success btn-block"><span class="fa fa-plus-circle"></span> Follow </button>
+                                     <!-- modal code start -->
+                                        <div class="modal fade shadow-lg p-3 mb-5 bg-white rounded" id="myModal">
+                                            <div class="modal-dialog modal-dialog-centered">
+                                                <div class="modal-content">
+
+                                                    <!-- Modal Header -->
+                                                    <div class="modal-header bg-primary">
+                                                        <h4 class="modal-title text-light">HireTruck Ad Post</h4>
+                                                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                                    </div>
+
+                                                    <!-- Modal body -->
+                                                    <div class="card-body">
+                                                        <h4 class="card-title">Post your AD for transfer your Luggage</h4>
+                                                        <p class="card-text">Fill up your requirement's for vehicle and Luggage and tap submit for open your ad
+                                                            in the HireTruck </p>
+                                                        <div class="container">
+                                                            <form action="" method="" class="form-group">
+                                                                <div class="form-group">
+                                                                    <label> Enter Source of Luggage</label>
+                                                                    <input type="textbox" name="source" class="form-control" placeholder="Ahemedabad" required/>
+                                                                </div>
+                                                                <div class="form-group">
+                                                                <label>Enter number of destination</label>
+                                                                <textarea class="form-control" placeholder="meghaninagar & maninagar" required>
+                                                                </textarea>
+                                                                </div>
+                                                                <div class="form-group">
+                                                                    <label>What kind of luggage you want to transfer</label>
+                                                                    <input type="textbox" name="luggage" class="form-control" placeholder="Furniture, Wood etc.." required/>
+                                                                </div>
+                                                                <div class="form-group">
+                                                                    <label>Type of luggage</label>
+                                                                    <input type="textbox" name="type_luggage" class="form-control" placeholder="Liquid, hard, soft, gas etc.." required/>
+                                                                </div>
+                                                                <div class="form-group">
+                                                                    <label>Waight of luggage in</label>&nbsp;<bold>Kg</bold>
+                                                                    <input type="number" name="waight" class="form-control" placeholder="1000Kg" required/>
+                                                                </div>
+                                                                <div class="form-group">
+                                                                    <label>Your Budget</label>
+                                                                    <input type="number" name="budget" class="form-control" placeholder="50000" required/>
+                                                                </div>
+                                                                <div class="form-group">
+                                                                    <label>Order date</label>
+                                                                    <input type="date" name="order_date" class="form-control" required/>
+                                                                </div>
+                                                                <div class="form-group">
+                                                                    <label>Vehicle type</label>
+                                                                    <div class="container">
+                                                                      <div class="row">
+                                                                        <div class="col-4">
+                                                                            <select class="form-conrol" name="wheel" required>
+                                                                              <option value="1">1</option>
+                                                                              <option value="1">1</option>
+                                                                              <option value="1">1</option>
+                                                                            </select>
+                                                                        </div>
+                                                                        <div class="col-4">
+                                                                          <select class="form-conrol" name="Waight" required>
+                                                                            <option value="2">2</option>
+                                                                            <option value="2">2</option>
+                                                                            <option value="2">2</option>
+                                                                          </select>
+                                                                        </div>
+                                                                        <div class="col-4">
+                                                                          <select class="form-conrol" name="Trk_type" required>
+                                                                            <option value="3">3</option>
+                                                                            <option value="3">3</option>
+                                                                            <option value="3">3</option>
+                                                                          </select>
+                                                                        </div>
+                                                                      </div>
+                                                                    </div>
+                                                                </div>
+                                                                <div>
+                                                                    <label>Extra Requirements</label>
+                                                                    <input type="textbox" name="extra_req" class="form-control" placeholder="Rope, Strip, Hooks, cable etc.." required/>
+                                                                </div>
+                                                                <br>
+
+
+                                                                <div class="container">
+                                                                    <div class="row">
+                                                                        <div class="col">
+                                                                            <button type="button" class="btn btn-secondary btn-block" data-dismiss="modal">
+                                                                                Close
+                                                                            </button>
+                                                                        </div>
+                                                                        <div class="col">
+                                                                            <input type="reset" value="reset" class="form-control btn btn-danger"/>
+                                                                        </div>
+                                                                        <div class="col">
+                                                                            <input type="Submit" value="submit" class="form-control btn btn-primary"/>
+                                                                            <div>
+                                                            </form>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                      </div></div></div></div>
+                                    <!-- ad modal end -->
+                                    <button class="btn btn-success btn-block" data-toggle="modal" data-target="#myModal"><span class="fa fa-plus-circle"></span> Post Ad  </button>
                                 </div><!--/col-->
                                 <div class="col-xs-12 col-sm-4">
                                     <h2><strong>245</strong></h2>
@@ -220,7 +323,7 @@ if($_SESSION['user_type']=="Transport"){ //transport condition started
         <script src="vendors/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
         <!-- Extra plugin css -->
         <script src="vendors/counterup/jquery.waypoints.min.js"></script>
-        <script src="vendors/counterup/jquery.counterup.min.js"></script> 
+        <script src="vendors/counterup/jquery.counterup.min.js"></script>
         <script src="vendors/counterup/apear.js"></script>
         <script src="vendors/counterup/countto.js"></script>
         <script src="vendors/owl-carousel/owl.carousel.min.js"></script>
@@ -230,7 +333,7 @@ if($_SESSION['user_type']=="Transport"){ //transport condition started
         <script src="vendors/circle-bar/plugins.js"></script>
         <script src="vendors/isotope/imagesloaded.pkgd.min.js"></script>
         <script src="vendors/isotope/isotope.pkgd.min.js"></script>
-        
+
         <script src="js/circle-active.js"></script>
         <script src="js/theme.js"></script>
     </body>
