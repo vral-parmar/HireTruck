973729f80267412befc48288913abf05b17564ba
KK-Gayatri-P <kkpg2014.pk@gmail.com>
2019-02-21 11:10:45 +0530

    Merge branch 'master' of https://github.com/parmarviral/HireTruck

--- Profile.php
+++ Profile.php
@@ -56,7 +56,6 @@ require_once("Session.php");
         <div class="container ">
             <div class="row card">
                 <div class="col-md-12 card-body">
-
                     <div class="panel panel-default">
                         <div class="panel-body">
                             <div class="row">
@@ -216,7 +215,50 @@ require_once("Session.php");
             </div>
         </div>
         <br>
-<?php }  //end of shipper condition
+        <?php
+        $ss=$_SESSION['mail'];
+        $query="SELECT * FROM ad  WHERE S_id=(SELECT S_id FROM user_s WHERE S_mail='$ss') AND status='0'";
+        $sql=mysqli_query($con,$query) or die(mysqli_error($con));
+        //print_r($sql);
+        //echo mysqli_num_rows($sql);
+        while($re=mysqli_fetch_array($sql)){
+        ?>
+        <div class="container card">
+          <div class="row well well-lg">
+            <div class="container"><br>
+              <h1 class="text-center">Ad Posted By you recently<h1>
+            </div>
+          </div>
+            <div class="container-fluid border">
+              <div class="row">
+                <div class="col-sm-6"><p>Source of Luggage : </p> <p> <?php  print $re[2];?></p>
+                  <p>Type of Luggage : </p> <p><?php  print $re[4];?>  </p>
+                  <p>Sub type of luggage : </p> <p> <?php  print $re[5];?>  </p>
+                  <p>Order Date of Luggage : </p> <p> <?php  print $re[9];?>  </p>
+                  <p>Extra Requirements for your Luggage : </p> <p> <?php  print $re[12];?>  </p></div>
+                <div class="col-sm-6"><p>Destination or Number of Drop Point : </p> <p><?php  print $re[3];?>  </p>
+                  <p>Waight of Luggage : </p> <p> <?php  print $re[6];?>  </p>
+                  <p>Approximate Prize for Transfer your Luggage : </p> <p> <?php  print $re[7];?>  </p>
+                  <p>Vehicle Type : </p> <p> <?php  print $re[11];?></p><br>
+                  <div class="row">
+                    <div class="col">
+                      <input type="Button" class="form-group btn btn-success btn-block" name="edit" value="Edit ad"/>
+                    </div>
+                    <div class="col">
+                      <input type="button" class="form-group btn btn-success btn-block" name="Delete_ad" value="Delete ad"/>
+                    </div>
+                    <div class="col">
+                      <input type="button" class="form-group btn btn-success btn-block" name="view_bid" value="Show Bid"/>
+                    </div>
+                  </div>
+                </div>
+
+                </div>
+              </div>
+            </div>
+          </div>
+      <br>
+<?php } } //end of shipper condition
 if($_SESSION['user_type']=="Transport"){ //transport condition started
 ?>        <!--================Banner Area =================-->
         <section class="banner_area">
