a4a071b4be85b10da9e6fb074a9d84771df33cca
PRINCE-SHAH <47053640+PRINCE-SHAH@users.noreply.github.com>
2020-01-18 22:06:45 +0530

    Update Deal_mail.php
    deal mail

--- Meiler/Deal_mail.php
+++ Meiler/Deal_mail.php
@@ -15,8 +15,8 @@ try {
     $mail->isSMTP();                                      // Set mailer to use SMTP
     $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'jamesmrt070@gmail.com';                 // SMTP username
-    $mail->Password = 'Hackie@789';                           // SMTP password
+    $mail->Username = 'enter your maid id';                 // SMTP username
+    $mail->Password = 'password';                           // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587;                                    // TCP port to connect to
 
